6e7a49336e3138f5e4585e0bec3e074ee5b0845c
Rahmanism <s.mostafa.rahmani@gmail.com>
2025-09-14 23:12:57 +0330

    Change api

--- src/api/urls.js
+++ src/api/urls.js
@@ -6,10 +6,10 @@ const ApiUrls = {
 
 export const getBaseUrl = (noApi = false) => {
   const apiPostfix = noApi ? '' : '/api'
-  const productionUrl = 'http://api.geolinks.ir'
+  const productionUrl = 'https://api.rahmanism.ir'
   // const localUrl = 'http://localhost:5059'
   // const localUrl = 'https://localhost:7059'
-  const localUrl = 'http://api.geolinks.ir'
+  const localUrl = 'https://api.rahmanism.ir'
   return (
     (process.env.NODE_ENV === 'production' ? productionUrl : localUrl) +
     apiPostfix
